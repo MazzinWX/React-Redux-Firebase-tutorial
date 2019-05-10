715f80d2339cf81193546b1d39bb1f123bc436c7
MazzinWx <meurant.m@gmail.com>
2019-05-10 16:35:17 +0200

    Update
    Adding code and going back to react-redux 5.1.1 to work

---
+++ marioplan/src/store/actions/projectActions.js
@@ -0,0 +1,17 @@
+export const createProject = (project) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        // make async call to db
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Mario',
+            authorLastName: 'Bros',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        })
+    }
+}
